44fd6d507fe06718ed8dae4c609a5d5a166535f0
G.Tjebbes <g.t@majerti.fr>
2017-10-26 18:01:50 +0200

    Update version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(os.path.join(here, 'README.txt')) as f:
 
 setup(
     name='sylk_parser',
-    version='0.2a1',
+    version='0.2',
     description='Basic SLK (sylk) to CSV parser',
     long_description=README,
     license='GPLv3',
