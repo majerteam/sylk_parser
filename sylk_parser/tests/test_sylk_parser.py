ac3a5863c7f9a9b07c8150c02b5dea6cc0fc8c1a
G.Tjebbes <g.t@majerti.fr>
2018-02-05 17:32:36 +0100

    Fix tests

--- sylk_parser/tests/test_sylk_parser.py
+++ sylk_parser/tests/test_sylk_parser.py
@@ -1,17 +1,27 @@
+import os
 from cStringIO import StringIO
 from sylk_parser import SylkParser
 from sylk_parser.sylk import SYLK
 
 
+def get_data_path(filename):
+    path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'datas')
+    return os.path.join(path, filename)
+
 def _test_one(test_filename, expected_results_filename, headers=None,
               encoding='cp1252'):
+
+    filepath = get_data_path(test_filename)
+    expected_results_filepath = get_data_path(expected_results_filename)
+
     if headers is None:
         headers = []
-    parser = SylkParser(test_filename, headers=headers, encoding=encoding)
+
+    parser = SylkParser(filepath, headers=headers, encoding=encoding)
     fbuf = StringIO()
     parser.to_csv(fbuf)
     test_results = fbuf.getvalue()
-    with open(expected_results_filename) as handle:
+    with open(expected_results_filepath) as handle:
         expected_results = handle.read()
     assert test_results.strip() == expected_results.strip()
     print("Tested {}".format(test_filename))
@@ -19,35 +29,36 @@ def _test_one(test_filename, expected_results_filename, headers=None,
 
 def test_to_csv():
     _test_one(
-        "datas/wikipedia.slk",
-        "datas/wikipedia.csv",
+        "wikipedia.slk",
+        "wikipedia.csv",
         encoding='utf-8',
     )
     headers = ["Utilisateur", "MotDePasse"]
     _test_one(
-        "datas/libreoffice.slk",
-        "datas/libreoffice.csv",
+        "libreoffice.slk",
+        "libreoffice.csv",
         headers=headers,
         encoding='utf-8',
     )
     _test_one(
-        "datas/balance_analytique.slk",
-        "datas/balance_analytique.csv",
+        "balance_analytique.slk",
+        "balance_analytique.csv",
         encoding='utf-8',
     )
     _test_one(
-        "datas/balance_analytique_cp1252.slk",
-        "datas/balance_analytique.csv",
+        "balance_analytique_cp1252.slk",
+        "balance_analytique.csv",
         encoding="cp1252",
     )
 
 
 def test_stream_unicode():
-    parser = SylkParser("datas/balance_analytique_cp1252.slk", use_unicode=True)
+    fpath = get_data_path("balance_analytique_cp1252.slk")
+    parser = SylkParser(fpath, use_unicode=True)
     for line in parser:
         for i in line:
             assert isinstance(i, unicode)
-    parser = SylkParser("datas/balance_analytique_cp1252.slk", use_unicode=False)
+    parser = SylkParser(fpath, use_unicode=False)
     for line in parser:
         for i in line:
             assert isinstance(i, str)
