5760cff3dfd8643a75f69b2e1cedc75797bfcc1f
G.Tjebbes <g.t@majerti.fr>
2018-02-05 17:39:42 +0100

    README

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(here, 'README.txt')) as f:
+with open(os.path.join(here, 'README.rst')) as f:
     README = f.read()
 
 setup(
